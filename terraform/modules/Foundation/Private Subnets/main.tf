cf7e40a920f4f1ab5110e30bcb1eb638bcf39566
K N Vijay <vijaybhargav1000@gmail.com>
2026-07-21 16:41:02 +0530

    Phase 2: Add public and private subnets

--- terraform/modules/Foundation/Private Subnets/main.tf
+++ terraform/modules/Foundation/Private Subnets/main.tf
@@ -1,7 +1,21 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${var.project_name}-${var.environment}-terraform-state"
+resource "aws_subnet" "private_1" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "Terraform State Bucket"
+    Name = "claimsiq-${var.environment}-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.12.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "claimsiq-${var.environment}-private-subnet-2"
   }
 }
