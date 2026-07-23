9d41adb18a5505eedafbfc533241c01f83e686be
K N Vijay <vijaybhargav1000@gmail.com>
2026-07-23 16:06:34 +0530

    Phase 8: Add Security Groups

---
+++ terraform/modules/Foundation/Security Groups/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "claimsiq-${var.environment}-ec2-sg"
+  description = "Application EC2 Security Group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "claimsiq-${var.environment}-ec2-sg"
+    Environment = var.environment
+  }
+}
--- terraform/modules/Foundation/Secutity Groups/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${var.project_name}-${var.environment}-terraform-state"
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
